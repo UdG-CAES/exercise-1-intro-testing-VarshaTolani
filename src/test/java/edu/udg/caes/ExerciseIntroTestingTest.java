85e460de5bbe7e5f6930dfb65b1c7c9897e1f2d9
varsha <varshatolani98@gmail.com>
2020-10-05 17:07:48 +0200

    New adapted tests passed.

--- src/test/java/edu/udg/caes/ExerciseIntroTestingTest.java
+++ src/test/java/edu/udg/caes/ExerciseIntroTestingTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ExerciseIntroTestingTest {
 
+    /* ORIGINAL TESTS */
     @Test
     public void testFindLast(){
         final int result = ExerciseIntroTesting.findLast(new int []{2, 3, 5}, 2);
@@ -28,5 +29,30 @@ public class ExerciseIntroTestingTest {
     public void testOddOrPos(){
         final int result = ExerciseIntroTesting.oddOrPos(new int []{-3, -2, 0, 1, 4});
         assertEquals(result, 3);
+    }
+
+    /* ADAPTED TESTS */
+    @Test
+    public void testFindLastAdapted(){
+        final int result = ExerciseIntroTesting.findLast(new int []{2, 3, 5}, 3);
+        assertEquals(result, 1);
+    }
+
+    @Test
+    public void testLastZeroAdapted(){
+        final int result = ExerciseIntroTesting.lastZero(new int []{0, 1});
+        assertEquals(result, 0);
+    }
+
+    @Test
+    public void testCountPositiveAdapted(){
+        final int result = ExerciseIntroTesting.countPositive(new int []{-4, 2, 2});
+        assertEquals(result, 2);
+    }
+
+    @Test
+    public void testOddOrPosAdapted(){
+        final int result = ExerciseIntroTesting.oddOrPos(new int []{-3, -2, 1, 4});
+        assertEquals(result, 2);
     }
 }
